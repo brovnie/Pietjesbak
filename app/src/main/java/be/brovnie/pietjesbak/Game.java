de0688538fed05163253d1ddeffdf4494514692f
Marlena <maarl.deenis@gmail.com>
2020-01-12 20:47:27 +0100

    Displays Score next to the player name.

--- app/src/main/java/be/brovnie/pietjesbak/Game.java
+++ app/src/main/java/be/brovnie/pietjesbak/Game.java
@@ -2,91 +2,163 @@ package be.brovnie.pietjesbak;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Handler;
 import android.content.Intent;
 import android.widget.TextView;
 import android.view.View;
 import android.graphics.Color;
 
+
 public class Game extends Activity {
 
     public static final String PLAYER1_NAME = "player1";
     public static final String PLAYER2_NAME = "player2";
-
+    private  TextView player1Score;
+    private TextView player2Score;
+    private TextView playerName1;
+    private TextView playerName2;
+    private TextView results;
+    private TextView bestScore;
+    private boolean firstGame;
+    private String playerText1;
+    private String playerText2;
     boolean player1;
-    boolean player2;
     int counter = 0;
     int max = 0;
     int totalScorePlayer1 = 0;
     int totalScorePlayer2 = 0;
     int best = 0;
-
+    final Handler handler = new Handler();
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
 
-        Intent intent = getIntent();
-        TextView playerName1 = (TextView)findViewById(R.id.player1);
-        String playerText1 = intent.getStringExtra(PLAYER1_NAME);
-        playerName1.setText(playerText1);
-        TextView playerName2 = (TextView)findViewById(R.id.player2);
-        String playerText2 = intent.getStringExtra(PLAYER2_NAME);
-        playerName2.setText(playerText2);
-        TextView player1Score = (TextView)findViewById(R.id.player1Score);
-        TextView player2Score = (TextView)findViewById(R.id.player2Score);
-        player1Score.setText(Integer.toString(totalScorePlayer1));
-        player2Score.setText(Integer.toString(totalScorePlayer2));
-        player1 = true;
-        player2 = false;
-    }
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_game);
+
+            Intent intent = getIntent();
+            playerName1 = (TextView) findViewById(R.id.player1);
+            playerText1 = intent.getStringExtra(PLAYER1_NAME);
+            playerName1.setText(playerText1);
+            playerName2 = (TextView) findViewById(R.id.player2);
+            playerText2 = intent.getStringExtra(PLAYER2_NAME);
+            playerName2.setText(playerText2);
+            player1Score = (TextView) findViewById(R.id.player1Score);
+            player2Score = (TextView) findViewById(R.id.player2Score);
+            player1Score.setText(Integer.toString(totalScorePlayer1));
+            player2Score.setText(Integer.toString(totalScorePlayer2));
+            results = (TextView)findViewById(R.id.roll_results);
+            bestScore = (TextView)findViewById(R.id.roll_bestScore);
+            player1 = true;
+            firstGame = true;
+        }
+
+        //Steps:
+        //KO 1. rzucic raz kostka kto wyrzuci wiecej zaczyna
+        //KO 2. p1 rzuca 3 kostkami
+        // KO     i. generuj 3 numery - random
+        // KO     ii.podlicz numery
+        // 3. p1 moze zdecydowac zeby rzucic jeszcze raz albo odpuscic
+        //      i. pojawia sie przycisk "stoefen" po kliknieciu tura p1 sie konczy
+        // 4. p1 wybiera iloma kostkami chce rzucic wszystkimi(3), 2, 1
+        //      i.
+        // 5. p1 rzuca kostkami
+        // 6. powtorzyc step 3,4,5 , p1 moze max 3 razy w ciagu tury rwucic kostkami
+        // 7. ostatni rezultat zostale
+        // 8. p1 to samo co p2 i moze tyle razy rwucic jak p1 (jesli on zatrzymuje sie na pierwszym razie p2 moze tlko 1 raz rzucic)
+        // 9. player ktory wyrzucil wieksza liczbe wygral i moze zdrapac jedena kreske
+        //      a) jesli rzucil 3 takie same numery moze zdrapac 2 kreski
+        // 10. wygrywa ten co nie ma juz dostepnych kresek
+        // extra buttons automatycznie wyrzuci zand, 3 appen
+
     // method ocClick
 
 public void onClickRollTheDice(View view){
-    TextView playerName1 = (TextView)findViewById(R.id.player1);
-    TextView playerName2 = (TextView)findViewById(R.id.player2);
-    TextView player1Score = (TextView)findViewById(R.id.player1Score);
+   // TextView playerName1 = (TextView)findViewById(R.id.player1);
+    //playerName2 = (TextView)findViewById(R.id.player2);
+    //player1Score = (TextView)findViewById(R.id.player1Score);
     TextView player2Score = (TextView)findViewById(R.id.player2Score);
-    player1Score.setText(Integer.toString(totalScorePlayer1));
-    player2Score.setText(Integer.toString(totalScorePlayer2));
-
     if(counter == 3){
         counter = 0;
-
         if(player1 == true){
             player1 = false;
-            player2 = true;
         } else {
             player1 = true;
-            player2 = false;
         }
     }
-       if(player1){
-            playerName1.setBackgroundColor(Color.GREEN);
-            playerName2.setBackgroundColor(Color.TRANSPARENT);
+    if(player1){
+        playerName1.setBackgroundColor(Color.GREEN);
+        playerName2.setBackgroundColor(Color.TRANSPARENT);
 
+    } else {
+        playerName2.setBackgroundColor(Color.GREEN);
+        playerName1.setBackgroundColor(Color.TRANSPARENT);
+    }
+    //First game
+    if(firstGame == true){
+        int[] arrNumbers = giveNumbers();
+        String n = arrNumbers[0] + " " + arrNumbers[1] + " " + arrNumbers[2];
+        results.setText(n);
+        if(player1==true){
+            totalScorePlayer1 = countResult(arrNumbers);
         } else {
-            playerName2.setBackgroundColor(Color.GREEN);
-            playerName1.setBackgroundColor(Color.TRANSPARENT);
+            totalScorePlayer2 = countResult(arrNumbers);
         }
-    TextView results = (TextView)findViewById(R.id.roll_results);
+        //change player
+        if(player1 == true){
+            player1 = false;
+        }
+        counter++;
+        if(counter == 2){
+            if(totalScorePlayer1 > totalScorePlayer2){
+                bestScore.setText(playerText1 +" begins!");
+                firstGame = false;
+                player1 = true;
+            } else if (totalScorePlayer1 < totalScorePlayer2){
+                bestScore.setText(playerText2 + " begins!");
+                firstGame = false;
+                player1 = false;
+            } else{
+                bestScore.setText("Draw! Let's try again");
+                firstGame = true;
+            }
+            counter = 0;
+            totalScorePlayer2 = 0;
+            totalScorePlayer2 = 0;
+        }
+        //ROLL DICE
+    } else {
+
     int[] arrNumbers = giveNumbers();
     String n = arrNumbers[0] + " " + arrNumbers[1] + " " + arrNumbers[2];
     results.setText(n);
     int sum = countResult(arrNumbers);
     TextView resultsSum = (TextView)findViewById(R.id.roll_sum);
     resultsSum.setText(Integer.toString(sum));
-    best = findBestResult(sum);
+        player1Score.setText(Integer.toString(totalScorePlayer1));
+        player2Score.setText(Integer.toString(totalScorePlayer2));
+ best = findBestResult(sum);
     TextView bestScore = (TextView)findViewById(R.id.roll_bestScore);
     bestScore.setText("Best score so far " + Integer.toString(best));
-    if(counter == 2) {
+    if(counter == 3) {
         if (player1 == true) {
             totalScorePlayer1 = totalScorePlayer2 + best;
         } else {
             totalScorePlayer2 = totalScorePlayer2 + best;
         }
     }
-    counter++;
-}
+    counter++; }
+} // end onClickRollTheDice
+    public void onClickStop(View view){
+        counter = 0;
+        if(player1 == true){
+            totalScorePlayer1 = totalScorePlayer2 + best;
+            player1 = false;
+        }else {
+            player1 = true;
+            totalScorePlayer2 = totalScorePlayer2 + best;
+        }
+
+    }
 
 public static int[] giveNumbers(){
     int[] dices = new int[3];
--- app/src/main/java/be/brovnie/pietjesbak/MainActivity.java
+++ app/src/main/java/be/brovnie/pietjesbak/MainActivity.java
@@ -5,8 +5,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
+    private EditText playerView1;
+    private EditText playerView2;
+    private String playername1;
+    private String playername2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,18 +22,23 @@ public class MainActivity extends Activity {
 
 public void onClickPlayersNames (View view){
         //Get data from Edit text field
-    EditText playerView1 = (EditText)findViewById(R.id.player1_name);
-    EditText playerView2 = (EditText)findViewById(R.id.player2_name);
-        //Convert to string
-    String playername1 = playerView1.getText().toString();
-    String playername2 = playerView2.getText().toString();
+         playerView1 = (EditText)findViewById(R.id.player1_name);
+         playerView2 = (EditText)findViewById(R.id.player2_name);
 
+        //Convert to string
+    playername1 = playerView1.getText().toString();
+    playername2 = playerView2.getText().toString();
     //Connect activities
     Intent intent = new Intent(this, Game.class);
-    //Using constants so both activities use the same name
-    intent.putExtra(Game.PLAYER1_NAME,playername1);
-    intent.putExtra(Game.PLAYER2_NAME,playername2);
-    //Start with the intent
-    startActivity(intent);
+    if(playername1.length() != 0 || playername2.length() != 0 ) {
+        //Using constants so both activities use the same name
+        intent.putExtra(Game.PLAYER1_NAME,playername1);
+        intent.putExtra(Game.PLAYER2_NAME,playername2);
+        //Start with the intent
+        startActivity(intent);
+    } else {
+        Toast.makeText(MainActivity.this, "Please enter player name", Toast.LENGTH_LONG).show();
+    }
+
 }
 } //end
